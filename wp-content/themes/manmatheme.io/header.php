acbd10e9f336c0131c0da042f352b8cf4f08d1c6
harhogefoo <harimanmon@gmail.com>
2016-11-12 20:37:15 +0900

    各ページで必要な画像のコピー Update .gitignore

--- wp-content/themes/manmatheme.io/header.php
+++ wp-content/themes/manmatheme.io/header.php
@@ -2,8 +2,8 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta charset="utf-8">
-  <!-- <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title> -->
-  <title>manma(マンマ)｜家族留学で子育てのリアルを学ぶ</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
   <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSSフィード" href="<?php bloginfo('rss2_url'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,28 +13,29 @@
   <meta property="og:title" content="【manma】家族留学｜子育てのリアルを学ぶ" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="manma.co" />
-  <meta property="og:image" content="./image/index/ogp/ogp280.png" />
+  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/image/index/ogp/ogp280.png" />
   <meta property="og:site_name" content="【manma】家族留学｜子育てのリアルを学ぶ" />
   <meta property="og:description" content="子育て中のご家庭を1日訪問。ママの生き方についてお話しを伺い、様々な家族のあり方を知ることを通して、自身のキャリアプランと向き合います。" />
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/image/logo/favicon.png">
+  <title><?php wp_title('｜', true, 'right'); ?><?php bloginfo('name'); ?></title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/ie-emulation-modes-warning.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.12.0.min.js"></script>
-  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/holder.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/ie10-viewport-bug-workaround.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/index.js"></script>
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 </head>
 <body>
   <header id="header" class="header header-right header-color sticky-header translucent-header">
     <div class="container">
       <div class="row header-inner">
         <div id="logo" class="col-xs-6 col-sm-12 col-md-3 image-center-sm text-left-xs clearfix">
-          <a class="logo" href="http://manma.co/">
+        <a class="logo" href="<?php echo get_site_url(); ?>">
             <img id="header_logo" src="<?php echo get_template_directory_uri(); ?>/image/logo/logo.png" srcset="<?php echo get_template_directory_uri(); ?>/image/logo/logo.png 1x, <?php echo get_template_directory_uri(); ?>/image/logo/logo@2x.png 2x" alt="manma">
           </a>
-          <a class="logo-translucent" href="http://manma.co/">
+          <a class="logo-translucent" href="<?php echo get_site_url(); ?>">
             <img id="header_logo" src="<?php echo get_template_directory_uri(); ?>/image/logo/logo.png" srcset="<?php echo get_template_directory_uri(); ?>/image/logo/logo.png 1x, <?php echo get_template_directory_uri(); ?>/image/logo/logo@2x.png 2x" alt="manma">
           </a>
         </div>
@@ -45,11 +46,11 @@
             </div>
             <div class="collapse navbar-collapse navbar-ex1-collapse">
               <ul class="nav navbar-right navbar-nav">
-                <li><a href="http://manma.co/media">MEDIA<br>メディア掲載一覧</a></li>
-                <li><a href="http://manma.co/team">TEAM<br>スタッフ一覧</a></li>
+                <li><a href="<?php echo get_site_url(); ?>/media/">MEDIA<br>メディア掲載一覧</a></li>
+                <li><a href="<?php echo get_site_url(); ?>/team/">TEAM<br>スタッフ一覧</a></li>
                 <li><a href="http://manma.co/blog/">BLOG<br>家族留学レポート</a></li>
-                <li><a href="http://manma.co/recruit">RECRUIT<br>スタッフ募集</a></li>
-                <li class="hide-xs"><a class="action-btn" href="http://manma.co/contact" target="_blank"><button class="button btn">CONTACT<br>お問い合わせ</button></a></li>
+                <li><a href="<?php echo get_site_url(); ?>/recruit/">RECRUIT<br>スタッフ募集</a></li>
+                <li class="hide-xs"><a class="action-btn" href="<?php echo get_site_url(); ?>/contact/" target="_blank"><button class="button btn">CONTACT<br>お問い合わせ</button></a></li>
               </ul>
             </div>
           </nav>
