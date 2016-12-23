3529e142baeae919e5a19216e267916bf1a0883b
harhogefoo <harimanmon@gmail.com>
2016-12-23 18:17:15 +0900

    メディア掲載一覧の各ページデザイン実装

--- wp-content/themes/manmatheme.io/single-medias.php
+++ wp-content/themes/manmatheme.io/single-medias.php
@@ -1,40 +1,43 @@
 <?php get_header();?>
 <!--------------MainContent--------------->
 <article id="main-content">
-<div class="grid"><!-- GRID MOTHER -->
-<section id="subpage" class="subpage">
-  <div class="text-center text-shadow">
-    <h3 class="subpage-title">MEDIA</h3>
-    <h5 class="subpage-subtitle">メディア掲載</h5>
+  <div class="grid">
+    <!-- key header -->
+    <section id="subpage" class="subpage">
+      <div class="text-center text-shadow">
+        <h3 class="subpage-title">MEDIA</h3>
+        <h5 class="subpage-subtitle">メディア掲載</h5>
+      </div>
+    </section>
+
+    <!-- content -->
+    <section id="media" class="section">
+      <div class="container">
+        <p><?php the_time('Y.n.j'); ?></p>
+        <h3><?php the_title(); ?></h3>
+        <div class="col col-lg-4">
+          <div class="img-center">
+            <?php the_post_thumbnail(); ?>
+          </div>
+        </div>
+        <div class="col col-lg-8">
+        <?php
+        if (have_posts()) :
+          while (have_posts()) :
+            the_post();
+            get_template_part('content');
+          endwhile;
+        endif;
+        ?>
+        <?php if( get_post_meta($post->ID, 'link', true) ) { ?>
+        関連記事: <a href="<?php echo get_post_meta($post->ID, 'link', true); ?>">
+         <?php echo get_post_meta($post->ID, 'link', true); ?></a>
+         <?php } ?>
+        </div>
+      </div>
+    </section>
   </div>
-</section>
-<section id="media" class="section">
-</section>
-<?php
-if (have_posts()) :
-  while (have_posts()) :
-    the_post();
-    get_template_part('content');
-  endwhile;
-endif;
-?>
-<!--------------RelatedPostLinks--------------->
-<div class="col-full">
-  <div class="wrap-col">
-    <div class="related_post">
-    <!-- <h1>関連記事</h1> -->
-    <!-- <?php get_template_part('single_related'); ?> -->
-    </div><!-- ^ .related_post END-->
-  </div><!-- ^ .wrap-col END-->
-</div><!-- ^ .col-full END-->
-<!-----------RelatedPostLinks END------------>
-<!--------------CommentArea--------------->
-<div class="col-full">
-  <div class="wrap-col">
-    <?php comments_template(); ?>
-  </div><!-- ^ .wrap-col END-->
-</div><!-- ^ .col-full END-->
-</div><!-- ^ .grid = GRID MOTHER END-->
-</article><!-- ^ main-content END-->
+</article>
+<!-- MainContent END -->
 <?php get_sidebar();?>
 <?php get_footer();?>
